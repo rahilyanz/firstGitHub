1ae82860c8f1cddcd1f3c2bd9255142b3d14d802
rahilyanz <rahaomurzakova@gmail.com>
2024-01-08 21:55:31 +0600

    Initial Commit

---
+++ firstGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstGitHub
+//
+//  Created by user on 8/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
